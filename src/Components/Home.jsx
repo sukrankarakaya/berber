83103740087fd4c605c1803beacb5108e8d568d6
EMRE TETİK <162701482+Emretetik0@users.noreply.github.com>
2024-03-26 16:48:46 +0300

    home için

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 
 const Home = () => {
@@ -8,27 +7,5 @@ const Home = () => {
     </div>
   )
 }
-=======
-import React from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-import BarberCard from "./BarberCard";
-const Home = () => {
-  return (
-    <div className="bg-light p-8 min-h-screen">
-      <div className="berber-cards flex flex-col justify-end  pl-50">
-        <div className=" grid grid-cols-2 gap-4 p-2 justify-center items-center ">
-          <BarberCard />
-          <BarberCard />
-          <BarberCard />
-          <BarberCard />
-          <BarberCard />
-          <BarberCard />
-        </div>
-      </div>
-    </div>
-  );
-};
->>>>>>> 2b462fca2554bf76049ca30553bd131725019d1b
 
 export default Home
