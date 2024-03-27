02070a4fa79555c16786a198f285582691ec1921
sukrankarakaya <sukrankrky184@gmail.com>
2024-03-28 00:24:31 +0300

    Customer Barber List end sliderbar

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RegisterModal from "./Components/RegisterModal";
 
 function App() {
   return (
-    <>
+    <div className="font-quicksand">
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/home" element={<Home />} />
@@ -16,7 +16,7 @@ function App() {
 
         <Route path="*" element="" />
       </Routes>
-    </>
+    </div>
   );
 }
 
--- src/Components/BarberCard.jsx
+++ src/Components/BarberCard.jsx
@@ -3,31 +3,30 @@ import berber2 from "/public/Image/barber2.jpg";
 import { BsFillStarFill } from "react-icons/bs";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-
-
+import bestBarber from "../mock/bestbarbers.json";
 
 const BarberCard = () => {
   return (
+    <div className=" grid grid-cols-2 gap-4 gap-x-12 ">
+      {bestBarber.map((barber) => (
+        <div className="flex flex-row p-2  rounded-xl w-[480px] h-36 border border-secondary text-gray-700 hover:bg-[#d8d5d2] hover:border-2 ">
+          <div className=" ">
+            <img src={berber2} alt="" className="w-32 h-full rounded-xl" />
+          </div>
+          <div className="flex flex-col gap-2 pl-5">
+            <h1 className="font-bold text-xl ">{barber.name}</h1>
+            <div className="flex  flex-row gap-1 items-center">
+              <FaMapMarkerAlt />
+              <p className=" "> {barber.location}</p>
+            </div>
 
-    
-    <div className="flex flex-row p-2  rounded-xl w-[550px] h-40 border-2 border-secondary hover:bg-[#DAD5CD] ">
-      <div className=" ">
-        <img src={berber2} alt="" className="w-32 h-full rounded-xl" />
-      </div>
-      <div className="flex flex-col gap-3 pl-5">
-        <h1 className="font-bold text-xl ">
-          Alana Barbershop - Haircut massage & Spa
-        </h1>
-        <div className="flex  flex-row gap-1 items-center">
-          <FaMapMarkerAlt />
-          <p className=" ">Banguntapan (5 km)</p>
+            <div className="flex  flex-row gap-1 items-center">
+              <BsFillStarFill className="text-yellow-600" />
+              <p className=" "> {barber.rating}</p>
+            </div>
+          </div>
         </div>
-
-        <div className="flex  flex-row gap-1 items-center">
-          <BsFillStarFill className="text-yellow-600" />
-          <p className=" ">4.5</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
