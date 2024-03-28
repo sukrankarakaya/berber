74040681891bee1011fbe950c15259e9fe7ab24e
ucark <ucarkudret7@gmail.com>
2024-03-28 10:15:26 +0300

    back-end

---
+++ back-end/Barber/Barber/BarberCreate.cs
@@ -0,0 +1,17 @@
+﻿namespace Barber
+{
+    public class BarberCreate
+    {
+        public string UserName { get; set; }
+        public string WorkPlaceName { get; set; }
+        public string Phone {  get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string TaxNo { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+        public string BuildingNo { get; set; }
+        public string DoorNumber { get; set; }
+    }
+}
---
+++ back-end/Barber/Barber/CustomerCreate.cs
@@ -0,0 +1,13 @@
+﻿namespace Barber
+{
+    public class CustomerCreate
+    {
+        public string UserName { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+        public string Street { get; set; }
+    }
+}
