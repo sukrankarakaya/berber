6cb3ce2a584a2a4ae5fc1f25fffb804d48781216
EMRE TETİK <162701482+Emretetik0@users.noreply.github.com>
2024-03-28 09:19:47 +0300

    timecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import WelcomePage from "./Layout/WelcomePage";
-import Homeberber from "./Components/Homeberber";
+import Homebarber from "./Components/Homebarber";
 import LoginModal from "./Components/LoginModal";
 import RegisterModal from "./Components/RegisterModal";
 
@@ -10,7 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        <Route path="/homeberber" element={<Homeberber />} />
+        <Route path="/homebarber" element={<Homebarber />} />
         <Route path="/login" element={<LoginModal />} />
         <Route path="/register" element={<RegisterModal />} />
 
--- src/Components/Timecard.jsx
+++ src/Components/Timecard.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const Timecard = () => {
   return (
-    <div>
-      <div className='bg-black text-2xl text-white rounded-t-lg py-3 pl-6'> Saatler</div>
-      <div className='bg-slate-900 '>
-        <div className='bg-white h-6 w-6 text-black'>08.30</div>
+    <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
+      <h5 className="mb-3 text-base font-semibold text-gray-900 md:text-xl dark:text-white bg-">
+        Saatler
+      </h5>
+      <div className="flex flex-wrap">
+        <div className="flex flex-col w-1/5">
+          <p className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+            08.30
+          </p>
+          <p className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+            09.00
+          </p>
+          <p className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+            09.30
+          </p>
+          <p className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+            10.00
+          </p>
+          <p className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+            10.30
+          </p>
+        </div>
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Timecard
+export default Timecard;
