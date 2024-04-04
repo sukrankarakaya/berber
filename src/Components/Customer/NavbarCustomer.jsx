2125ddbe12d44a6f5998dea227c2d8229e9cf58a
sukrankarakaya <sukrankrky184@gmail.com>
2024-04-04 10:10:21 +0300

    dropdown
    .

--- src/Components/Customer/NavbarCustomer.jsx
+++ src/Components/Customer/NavbarCustomer.jsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Dropdown from '../Dropdown';
+import Dropdown from '../DropdownCustomer';
 import Location from '../Location';
 
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Logo from '../Components/Logo';
-import Dropdown from './Dropdown';
+import Dropdown from './DropdownCustomer';
 import Location from './Location';
 
 
